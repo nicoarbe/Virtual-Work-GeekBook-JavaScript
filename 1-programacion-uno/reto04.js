c3f75a1b0a25f2f7de697850a1ca12ee6494a6c1
Nicolás Arbeláez <nicoarbelaez@gmail.com>
2020-11-18 23:19:21 -0500

    Ejercicio 4 segunda parte

--- 1-programacion-uno/reto04.js
+++ 1-programacion-uno/reto04.js
@@ -1,3 +1,4 @@
 var listElement = document.querySelector('li');
 var titleElement = document.querySelector('h1');
+listElement = document.querySelector('#list');
 titleElement.innerHTML = titleElement.innerHTML + ' ' + listElement.innerHTML;
