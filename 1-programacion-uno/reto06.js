4c8ff01c35ce1fc14233dbfe7947eb2866a4c766
Nicolás Arbeláez <nicoarbelaez@gmail.com>
2020-11-19 23:02:37 -0500

    Ejercicio 6

---
+++ 1-programacion-uno/reto06.js
@@ -0,0 +1,5 @@
+const loremElement = document.querySelector('.lorem');
+
+const content = '<h1>Lorem ipsum</h1><img src="http://via.placeholder.com/350x150" alt=""><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+
+loremElement.innerHTML = content;
