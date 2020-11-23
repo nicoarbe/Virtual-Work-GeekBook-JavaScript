ffc69e65ee9162c3cf75145fa1219c61b4f3c319
Nicolás Arbeláez <nicoarbelaez@gmail.com>
2020-11-22 21:33:16 -0500

    Ejercicio 4

---
+++ 2-programacion-2/reto04.js
@@ -0,0 +1,6 @@
+const cuentaTotal = 128;
+const numPersonas = 9;
+let valorPersona = (cuentaTotal - 2) / numPersonas;
+let valorAna = valorPersona + 2;
+
+console.log('El valor a pagar por persona es de ' + valorPersona + ' Euros y Ana debe pagar ' + valorAna + ' Euros');
