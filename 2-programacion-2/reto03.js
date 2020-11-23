6669d88e11f86337e1197738b15652333a31645b
Nicolás Arbeláez <nicoarbelaez@gmail.com>
2020-11-22 21:14:02 -0500

    Ejercicio 3

---
+++ 2-programacion-2/reto03.js
@@ -0,0 +1,6 @@
+const kiwis = 5;
+const peras = 2;
+const uvas = 4;
+
+let precioTotal = 2 * kiwis + 3 * peras + 0.5 * uvas;
+console.log('El precio total de la compra de frutas es de: ' + precioTotal + ' Euros');
