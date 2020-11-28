5624d165152c803c0f3032cfd863b24b9648d87e
NICOLAS ARBELAEZ <nicoarbelaez@gmail.com>
2020-11-27 21:25:04 -0500

    Ejercicio 2

---
+++ 3-condicionales/reto02.js
@@ -0,0 +1,9 @@
+let userName = 'Juan Peláez';
+const myName = 'Nicolás Arbeláez';
+const hisName = 'Juan Peláez';
+
+if (myName === userName || hisName === userName) {
+    console.log(`Bienvenido, ${userName}`)
+} else {
+    console.log(`Lo siento pero el usuario que has introducido no está registrado`)
+}
