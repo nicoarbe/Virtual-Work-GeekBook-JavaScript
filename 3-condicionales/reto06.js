bb035031104477523be8e649e0e2fc2ce1bbd335
NICOLAS ARBELAEZ <nicoarbelaez@gmail.com>
2020-11-29 20:51:04 -0500

    Ejercicio 6

---
+++ 3-condicionales/reto06.js
@@ -0,0 +1,5 @@
+let avocados;
+const avocadoPrice = 1.5;
+const money = 33;
+
+(money >= avocadoPrice) ? console.log(`Puedes comprar ${avocados = money / avocadoPrice} aguacates`) : console.log(`Puedes comprar ${avocados = 0} aguacates`);
