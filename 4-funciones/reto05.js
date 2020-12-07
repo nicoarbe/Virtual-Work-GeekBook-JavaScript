d3352b9cf30861a25b1bb5212efd86828dcf9333
NICOLAS ARBELAEZ <nicoarbelaez@gmail.com>
2020-12-06 22:32:28 -0500

    Ejercicio 5

---
+++ 4-funciones/reto05.js
@@ -0,0 +1,20 @@
+let borderBox = true;
+let width = 600;
+let padding = 15;
+let border = 5;
+
+const calculate = (borderBox, width, padding, border) => {
+    let acum = 0;
+    if (borderBox === true) {
+        acum = width + padding + border;               
+    }else {
+        acum = width;
+    }
+    return acum;        
+}
+
+if (borderBox === true) {
+    console.log("El ancho de la caja es " + calculate(borderBox, width, padding, border));
+}else {
+    console.log("El ancho del contenido es " + calculate(borderBox, width, padding, border));
+}
